7673d0317c8ba3489982414417422d0d6fcb5a06
blackingg <mudet04@gmail.com>
2024-09-22 14:28:42 +0100

    #

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import SubscriptionCalendar from "./components/SubscriptionCalender";
 
 function App() {
   return (
-    <>
+    <div className="h-screen w-screen">
       <SubscriptionCalendar />
-    </>
+    </div>
   );
 }
 
